e36a19e53c183e08db60497ac8d02fc1c0a41495
terrazas22 <terrazasshuamann22@gmail.com>
2023-12-07 16:47:23 -0500

    añadir configuracion al servidor

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express = require("express")
+const cors = require("cors")
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(cors())
+
+app.listen(3000, (err)=>{
+    if(err) console.log("an error ocurred")
+    console.log("server running on port 3000")
+})
